0d65278887c25b7dec24485d848724b7f2662fc9
arnav10000 <83585771+arnav10000@users.noreply.github.com>
2021-06-09 13:21:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function more_rows(){
+    document.getElementById("more_rows_1").innerHTML ="Most of space is relatively empty, meaning that there are just stray bits of dust and gas inside of it. This means that when humans send a satellite to a distant planet, the object will not encounter darg in the same way that an airplane does as it sails through space.";
+    document.getElementById("more_rows_2").innerHTML ="The vacuum environment in space and on the moon, for example, is one reason that the lunar lander of the Apollo program looks so odd-shaped — like a spider, one crew said. Because the spacecraft was designed to work in a zone with no atmosphere, there was no need for smooth edges or an aerodynamic shape.";
+    document.getElementById("more_rows_3").innerHTML ="While space may look empty to human eyes, research has shown that there are forms of radiation emanating through the cosmos. In our own solar system, the solar wind — made up of plasma and other particles from the sun — permeates past the planets and occasionally causes aurora near the Earth's poles. Cosmic rays also fly through the neighborhood, emanating from supernovas outside of the solar system.";
+    document.getElementById("more_rows_4").innerHTML ="In fact, the universe is permeated with the cosmic microwave background, which can be understood as the leftovers of the immense explosion that formed our cosmos (usually called the Big Bang). The CMB, which is best seen in microwaves, shows the earliest radiation that our instruments can detect. [Infographic: Cosmic Microwave Background Explained]";
+    document. getElementById("more_rows"). style. display = "none";
+        var remove = document.getElementById("br_1");
+        remove.remove();
+        var remove = document.getElementById("br_2");
+        remove.remove();
+
+}
